1e01d308c64743f3798c2920ce454f3d7a608b17
jmukamani <j.mukamani@.com>
2023-07-16 11:49:49 +0000

    api

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -10,19 +10,15 @@ def recurse(subreddit, hot_list=[], after=""):
     param = {'after': after}
     response = requests.get(url, headers=header, params=param)
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data is not None:
-            children = data.get('children')
-            if children is not None:
-                for child in children:
-                    hot_list.append(child.get('data').get('title'))
-                after = data.get('after')
-                if after is not None:
-                    return recurse(subreddit, hot_list, after)
-                else:
-                    return hot_list
-        else:
-            return hot_list
+    if response.status_code != 200:
+	return None
     else:
-        return None
+	json_res = response.json()
+	after = json_res.get('data').get('after')
+	has_next = \
+            json_res.get('data').get('after') is not None
+	hot_articles = json_res.get('data').get('children')
+        [hot_list.append(article.get('data').get('title'))
+         for article in hot_articles]
+	return recurse(subreddit, hot_list, after=after) \
+            if has_next else hot_list
